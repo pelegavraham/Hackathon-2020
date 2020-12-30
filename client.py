80515fbfab72e123514cdd0a69b7315f8f611e92
Peleg Avraham <37583460+pelegavraham@users.noreply.github.com>
2020-12-30 20:26:28 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 from struct import *
 import sys
 
@@ -6,9 +7,8 @@ import sys
 class Client:
 
     def __init__(self, team_name):
-        self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # to get servers offer
-        self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # to send team name and play the game
-        self.host = socket.gethostname()
+        self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # to get servers offer
+        self.host = socket.gethostbyname(socket.gethostname())
         self.udp_port = 13117  # The same port as used by the servers
         self.team_name = team_name
         self.game_mode = False
@@ -18,19 +18,26 @@ class Client:
         self.udp_sock.bind((self.host, self.udp_port))
 
         while True:
-            data, address = self.udp_sock.recvfrom(7)  # received offer
+            print("want to recieve")
+            data, address = self.udp_sock.recvfrom(10)  # received offer
+            print("recieve....")
             magic_cookie, msg_type, tcp_port = unpack('Ibh', data)
             if is_valid(magic_cookie, msg_type):
-                print(f"Received offer from {address}, attempting to connect...")
-                self.connect(address, tcp_port)
+                print(f"Received offer from {address[0]}, attempting to connect...")
+                self.connect(address[0], 5050)
 
     def connect(self, address, tcp_port):  # connect TCP to send name and play
+        self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # to send team name and play the game
         self.tcp_sock.connect((address, tcp_port))
+
         try:
             self.tcp_sock.sendall(str.encode(self.team_name + '\n'))  # send team_name
-            while True:
+            end_time=time.time()+10
+            while time.time()<end_time:
                 # wait for a game started message
-                start_game_msg = self.tcp_sock.recv(1024).decode('utf-8')
+                print("want start msg..")
+                start_game_msg = self.tcp_sock.recv(4096).decode('utf-8')
+                print(start_game_msg)
                 if start_game_msg:
                     self.game_mode = True
                     print(start_game_msg)
@@ -43,6 +50,7 @@ class Client:
                             self.game_mode = False
 
         finally:
+            self.tcp_sock.close()
             print("Server disconnected, listening for offer requests...")
 
 
